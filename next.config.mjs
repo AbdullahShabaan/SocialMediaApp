e712350449452dd01a1d7a63da74d22b4a737fe7
AbdullahShabaan <131316365+AbdullahShabaan@users.noreply.github.com>
2024-09-10 07:55:04 +0300

    first commit 2

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
     ],
   },
   output: "export",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
 };
 
 export default nextConfig;
